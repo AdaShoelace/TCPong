e20789fef289c983c4023c2e10468a121be05d2c
PierreLeidbring <pierre.leidbring@gmail.com>
2017-06-18 00:39:33 +0200

    stuff and things

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
+    
+    const int PADDLE_STEP = 80;
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong", sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
+    window.setVerticalSyncEnabled(true);
     sf::Event event;
+    
+    sf::Vector2u screenSize = window.getSize();
+    const int SCREEN_WIDTH = screenSize.x;
+    std::cout << SCREEN_WIDTH << " width " << std::endl;
+    const int SCREEN_HEIGHT = screenSize.y;
+    std::cout << SCREEN_HEIGHT << " height " << std::endl;
 
+    sf::RectangleShape leftPaddle(sf::Vector2f(30, 100));
+    sf::Vector2f leftPaddlePos((float)leftPaddle.getSize().x,(float)SCREEN_HEIGHT / 2 - leftPaddle.getSize().y / 2);
+
+
+    sf::RectangleShape rightPaddle(sf::Vector2f(30, 100));
+    sf::Vector2f rightPaddlePos((float)SCREEN_WIDTH - rightPaddle.getSize().x * 2, (float)SCREEN_HEIGHT / 2 - leftPaddle.getSize().y / 2);
+
+    std::cout << leftPaddlePos.x << " " << leftPaddlePos.y << std::endl;
+
+    sf::Clock clock;
+    sf::Time deltaTime;
 
     while(window.isOpen())
     {
@@ -16,8 +37,22 @@ int main()
             {
                 window.close(); 
             }
+            
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                leftPaddlePos.y += PADDLE_STEP;
+            }
+            else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                leftPaddlePos.y -= PADDLE_STEP;
+            }
         }
+        deltaTime = clock.restart();
+        leftPaddle.setPosition(leftPaddlePos);
+        rightPaddle.setPosition(rightPaddlePos);
         window.clear();
+        window.draw(leftPaddle);
+        window.draw(rightPaddle);
         window.display();
     }
 
