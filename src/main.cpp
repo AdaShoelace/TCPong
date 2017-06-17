5248c2f0df12042eb93b4dc141fe134cceff6f8a
PierreLeidbring <pierre.leidbring@gmail.com>
2017-06-17 23:22:22 +0200

    close window

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,10 @@ int main()
     {
         while(window.pollEvent(event))
         {
-        
+            if(event.type == sf::Event::Closed)
+            {
+                window.close(); 
+            }
         }
         window.clear();
         window.display();
